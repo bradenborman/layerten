203f6b3b06316680ddbd37fc1dfef9a52ae81a59
Braden Borman <bradenborman@hotmail.com>
2026-01-18 16:15:57 -0600

    Update DevlServer to reference LayerTenApplication instead of being a SpringBootApplication

--- server/src/test/java/com/layerten/DevlServer.java
+++ server/src/test/java/com/layerten/DevlServer.java
@@ -1,7 +1,6 @@
 package com.layerten;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Development server launcher for local testing.
@@ -11,11 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * - In IntelliJ: Right-click this file and select "Run 'DevlServer.main()'"
  * - From command line: ./gradlew :server:bootRun --args='--spring.profiles.active=devl,local'
  */
-@SpringBootApplication
 public class DevlServer {
     
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(DevlServer.class);
+        SpringApplication app = new SpringApplication(LayerTenApplication.class);
         app.setAdditionalProfiles("devl", "local");
         app.run(args);
     }
